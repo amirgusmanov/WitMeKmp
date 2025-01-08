20c368fff477dd296beb7c3233596fa955c96058
amir-gusmanov <a.gusmanov30@gmail.com>
2025-01-08 23:33:47 +0500

    todo

--- core/navigation/src/commonMain/kotlin/kz/witme/project/navigation/Destination.kt
+++ core/navigation/src/commonMain/kotlin/kz/witme/project/navigation/Destination.kt
@@ -13,7 +13,7 @@ sealed interface Destination : ScreenProvider {
     data object Dashboard : Destination
     data object CreateBook : Destination
     data object Profile : Destination
-    data class CreateStatusBook(val args: CreateBookArgs) : Destination
+    data class CreateStatusBook(val args: CreateBookArgs) : Destination //todo check here, try to save image in repository
     data class Timer(
         val bookId: String? = null,
         val isNavigatedFromTabs: Boolean = false
