3a2567976239acf61bb141f512af0a47511a91a0
amirgusmanov <a.gusmanov30@gmail.com>
2025-01-08 15:40:56 +0500

    change base url

--- feature/splash/src/commonMain/kotlin/kz/witme/project/splash/SplashViewModel.kt
+++ feature/splash/src/commonMain/kotlin/kz/witme/project/splash/SplashViewModel.kt
@@ -5,6 +5,7 @@ import cafe.adriel.voyager.core.model.screenModelScope
 import kotlinx.coroutines.launch
 import kz.witme.project.common.extension.tryToUpdate
 import kz.witme.project.data.network.HttpClientFactory
+import kz.witme.project.data.network.onError
 import kz.witme.project.data.network.onSuccess
 import kz.witme.project.service.auth.domain.repository.AuthRepository
 
@@ -18,11 +19,17 @@ internal class SplashViewModel(
 
     private fun navigateUser() {
         screenModelScope.launch {
-            authRepository.navigateUser().onSuccess {
-                HttpClientFactory.navigateFlow.tryToUpdate {
-                    HttpClientFactory.NavigateFlow.TabsFlow
+            authRepository.navigateUser()
+                .onSuccess {
+                    HttpClientFactory.navigateFlow.tryToUpdate {
+                        HttpClientFactory.NavigateFlow.TabsFlow
+                    }
                 }
-            }
+                .onError {
+                    HttpClientFactory.navigateFlow.tryToUpdate {
+                        HttpClientFactory.NavigateFlow.LoginFlow
+                    }
+                }
         }
     }
 }
