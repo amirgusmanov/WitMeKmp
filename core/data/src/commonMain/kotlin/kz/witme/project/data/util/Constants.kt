fa2832c68cd095a30fbb6a9000b61f58806bb82c
amirgusmanov <a.gusmanov30@gmail.com>
2025-01-13 19:34:15 +0500

    update base url

--- core/data/src/commonMain/kotlin/kz/witme/project/data/util/Constants.kt
+++ core/data/src/commonMain/kotlin/kz/witme/project/data/util/Constants.kt
@@ -2,7 +2,7 @@ package kz.witme.project.data.util
 
 //todo move to gradle files when deploy
 internal object Constants {
-    const val BASE_URL = "https://witbook.amelia.monster/"
+    const val BASE_URL = "https://witbook.vivian.monster/"
     const val NETWORK_LOG_TAG = "Network"
     const val ACCESS = "Access_token"
     const val REFRESH = "Refresh_token"
