a63f969772afb163aa3793c44d07663f79c32dbf
amirgusmanov <a.gusmanov30@gmail.com>
2025-01-17 10:32:26 +0500

    fix request

--- feature/splash/src/commonMain/kotlin/kz/witme/project/splash/SplashViewModel.kt
+++ feature/splash/src/commonMain/kotlin/kz/witme/project/splash/SplashViewModel.kt
@@ -3,6 +3,9 @@ package kz.witme.project.splash
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import kotlinx.coroutines.launch
+import kz.witme.project.common.extension.tryToUpdate
+import kz.witme.project.data.network.HttpClientFactory
+import kz.witme.project.data.network.onSuccess
 import kz.witme.project.service.auth.domain.repository.AuthRepository
 
 internal class SplashViewModel(
@@ -16,6 +19,11 @@ internal class SplashViewModel(
     private fun navigateUser() {
         screenModelScope.launch {
             authRepository.navigateUser()
+                .onSuccess {
+                    HttpClientFactory.navigateFlow.tryToUpdate {
+                        HttpClientFactory.NavigateFlow.TabsFlow
+                    }
+                }
         }
     }
 }
