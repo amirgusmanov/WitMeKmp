f1c9c689643c66d0e62752bfedd683f9b156999a
amirgusmanov <a.gusmanov30@gmail.com>
2025-03-09 06:03:44 +0500

    update android version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -72,8 +72,8 @@ android {
         applicationId = "kz.witme.project"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
     packaging {
         resources {
